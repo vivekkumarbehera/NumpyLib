4cc7a9d7f65aa62275f5f83ba30753bfb8a4870c
vivekkumarbehera <vivekbehera0807@gmail.com>
2026-02-07 09:30:20 +0530

    Fix: rename list variable, add print statements, fix comments

--- AdvPythonDay3/Dictionart/Operation_of_L,T,S,D.py
+++ AdvPythonDay3/Dictionart/Operation_of_L,T,S,D.py
@@ -1,14 +1,24 @@
-#Update of the set,tuple, dictionary and list
+#Update of the set, tuple, dictionary and list
 #List operations
-list=[]
-type(list)
-print(list)
-list.extend([1,2,3,4,5]) #Adding multiple elements to the list  
-print(list)
-list.append(6) #Adding a single element to the list
+my_list = []
+print(type(my_list))
+print(my_list)
+my_list.extend([1,2,3,4,5]) #Adding multiple elements to the list  
+print(my_list)
+my_list.append(6) #Adding a single element to the list
+print(my_list)
 
+#Set operations
+my_set = set()
+print(type(my_set))
+print(my_set)
+my_set.update([1, 2, 3, 4, 5]) #Adding multiple elements to the set
+print(my_set)
 
+#Removing an element from the set
+my_set.remove(3)
+print(my_set)
 
-set1=(1,2,"hi")
-print(set1)
-
+#Discard an element from the set
+my_set.discard(4)
+print(my_set)
