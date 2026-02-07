8e781c4e1e27a62a3324c9b39aa549c5512be0ea
vivekkumarbehera <vivekbehera0807@gmail.com>
2026-02-07 09:43:48 +0530

    Add set operations example

---
+++ AdvPythonDay3/Dictionart/Operation_Of_Set.py
@@ -0,0 +1,14 @@
+#Set Operations
+my_set = set()
+print(type(my_set))
+print(my_set)
+my_set.add(1)
+my_set.add(2)
+my_set.add(3)
+my_set.add(4)
+my_set.add(5) #Adding multiple elements to the set
+print(my_set)
+my_set.clear() #Clearing the set
+print(my_set)
+my_set.discard(3) #Removing an element from the set
+print(my_set)
