cc42e85478a72ee091f1c5e56e20e74278ec8c62
vivekkumarbehera <vivekbehera0807@gmail.com>
2026-02-07 09:50:56 +0530

    Update Operation_of_L,T,S,D.py

--- AdvPythonDay3/Dictionart/Operation_of_L,T,S,D.py
+++ AdvPythonDay3/Dictionart/Operation_of_L,T,S,D.py
@@ -7,18 +7,5 @@ my_list.extend([1,2,3,4,5]) #Adding multiple elements to the list
 print(my_list)
 my_list.append(6) #Adding a single element to the list
 print(my_list)
-
-#Set operations
-my_set = set()
-print(type(my_set))
-print(my_set)
-my_set.update([1, 2, 3, 4, 5]) #Adding multiple elements to the set
-print(my_set)
-
-#Removing an element from the set
-my_set.remove(3)
-print(my_set)
-
-#Discard an element from the set
-my_set.discard(4)
-print(my_set)
+list=[1,2,3,4,5]
+print(list)
