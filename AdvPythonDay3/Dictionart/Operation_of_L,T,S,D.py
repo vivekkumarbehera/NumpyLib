7e18f83de29efc906737e13cf5bb3c4455a55980
vivekkumarbehera <vivekbehera0807@gmail.com>
2026-02-07 09:26:08 +0530

    Add operations of List, Tuple, Set, Dictionary

---
+++ AdvPythonDay3/Dictionart/Operation_of_L,T,S,D.py
@@ -0,0 +1,14 @@
+#Update of the set,tuple, dictionary and list
+#List operations
+list=[]
+type(list)
+print(list)
+list.extend([1,2,3,4,5]) #Adding multiple elements to the list  
+print(list)
+list.append(6) #Adding a single element to the list
+
+
+
+set1=(1,2,"hi")
+print(set1)
+
