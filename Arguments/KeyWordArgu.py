83db84287094c802505f820e75605d32d2bef7a2
vivekkumarbehera <vivekbehera0807@gmail.com>
2026-02-14 09:40:08 +0530

    KeywordArguments

---
+++ Arguments/KeyWordArgu.py
@@ -0,0 +1,5 @@
+#KeyWord Arguments
+def Stu(name,age,rollno,section):
+    print(f"Hello Good Morning !! My nae is {name} and my age is {age} and my rollno is {rollno} and my section is {section}")
+Stu(name='Aniket',age=22,rollno=101,section='I')
+Stu(rollno=101,section='I',name='Aniket',age=22)
