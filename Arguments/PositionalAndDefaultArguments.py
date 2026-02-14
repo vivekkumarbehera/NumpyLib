4f8ca6936f0a917a9f1f57f5fb897dae57ee9367
vivekkumarbehera <vivekbehera0807@gmail.com>
2026-02-14 09:11:07 +0530

    Add arguments examples

---
+++ Arguments/PositionalAndDefaultArguments.py
@@ -0,0 +1,4 @@
+def Stu(name,age,rollno,section='I'):
+    print(f"Hello Good Morning !! My nae is {name} and my age is {age} and my rollno is {rollno} and my section is {section}")
+Stu('Aniket',22,101)
+Stu('Aniket',22,101,'A')
