49093e5b496e67f232167b84b32b2154f6d2769a
vivekkumarbehera <vivekbehera0807@gmail.com>
2026-02-14 11:20:42 +0530

    Basic

---
+++ Arguments/Arbitary_KeyWord_Argu.py
@@ -0,0 +1,5 @@
+#Arbitary_KeyWord Arguments
+def Stu(name,age,rollno,section):
+    print(f"Hello Good Morning !! My name is {name} ")
+Stu(name='Aniket',age=22,rollno=101,section='I')
+Stu(rollno=101,section='I',name='vivek',age=22)
