ffdd792948e001acc14204c4d38d364d10156b64
vivekkumarbehera <vivekbehera0807@gmail.com>
2026-02-24 09:45:02 +0530

    employment details

---
+++ Arguments/Employment Details.py
@@ -0,0 +1,7 @@
+def EmploymentDetails(name,age,depatment="Tirupati Nagar 6th Line"):
+    print(f"Name:{name}")
+    print(f"Age:{age}")
+    print(f"Department:{depatment}")
+EmploymentDetails("Vicky King",22)
+EmploymentDetails("Rahul",22,"Sridhar Nagar 1st Line")
+    
