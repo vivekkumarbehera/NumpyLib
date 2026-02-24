853b7d2839663d37bf6e40f239f8805ce90b7378
vivekkumarbehera <vivekbehera0807@gmail.com>
2026-02-24 09:29:48 +0530

    online bill payment python program

--- Arguments/OnlineBillPayment.py
+++ Arguments/OnlineBillPayment.py
@@ -1,9 +1,9 @@
 def online_bill_payment(name,price,discount=10/100):
-    total=price
+    total= sum(price)
     discount_amount=(discount/100)*total
     final_price=total-discount_amount
     print(f"Name:{name}")
     print(f"Total:{total}")
     print(f"Discount Amount:{discount_amount}")
     print(f"Final Price:{final_price}")
-    online_bill_payment('Vicky King',1000)
+online_bill_payment('Vicky King',[100,2000])
