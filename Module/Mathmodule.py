8a5183ad190eba0a1f4ec6302fc24a4a231e1179
vivekkumarbehera <vivekbehera0807@gmail.com>
2026-02-24 11:18:12 +0530

    small Module we have do

---
+++ Module/Mathmodule.py
@@ -0,0 +1,6 @@
+import math
+print(dir(math))
+print(math.factorial(5))
+print(math.sqrt(16))
+print(math.pow(2,3))
+print(math.pi)
--- Module/importSample.py
+++ Module/importSample.py
@@ -1,2 +1,10 @@
-import myModule
-print(myModule.add(2,3))
+import myModule as md 
+print(md.sub(10,5))
+print(md.mul(10,5))
+print(md.div(10,5))
+print(md.add(10,5))
+print(dir(md))
+
+for i in dir(md):
+    print(i)    
+    
