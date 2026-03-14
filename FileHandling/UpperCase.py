f3416cd9c1c5a727cb2becc814509a9923b001c1
VivekKumarbehra <vivekbehera0807@gmail.com>
2026-03-14 11:18:12 +0530

    list to Dictionary

--- FileHandling/UpperCase.py
+++ FileHandling/UpperCase.py
@@ -1,3 +1,4 @@
+import math
 list1=['VICKY','PYTHON','NUMPY','PANDAS']
 list2=[i.lower() for i in list1]
 print(list2)
@@ -14,3 +15,9 @@ for i in range(3):
 print(list)
 list5=[(i,j) for i in range(3) for j in range(3)]
 print(list5)
+#list to dictionary
+list4=[1,4,9,26,25]
+dixt={i:math.sqrt(i) for i in list4}
+print(dixt)
+dict={i:i**2 for i in list4 if i%2==0}
+print(dict)
